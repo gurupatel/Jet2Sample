d1df156e1561c08d6191b16eba1f73da31a9f778
Gaurang <gurupatel.2684@gmail.com>
2020-02-25 21:42:01 +0530

    Added WebServices model class for calling API's

---
+++ Jet2Travel/Jet2Travel/Model/WebServices.swift
@@ -0,0 +1,15 @@
+//
+//  WebServices.swift
+//  Jet2Travel
+//
+//  Created by Gaurang Patel on 25/02/20.
+//  Copyright © 2020 Jet2Travel. All rights reserved.
+//
+
+import Foundation
+
+class WebServices {
+
+    // MARK: - Class Methods
+
+}
