152f63794d177fb148b4542d7c383dff1cc465a5
Gaurang <gurupatel.2684@gmail.com>
2020-02-25 22:05:54 +0530

    - Added Constant file for common declaration - Added getEmployeeData method in WebService model class to get Employee data - Added EmployeeDataCell UITableViewCell for showing employees list in a table view

---
+++ Jet2Travel/Jet2Travel/View/Cells/EmployeeDataCell.swift
@@ -0,0 +1,29 @@
+//
+//  EmployeeDataCell.swift
+//  Jet2Travel
+//
+//  Created by Gaurang Patel on 25/02/20.
+//  Copyright © 2020 Jet2Travel. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - EmployeeData Cell
+
+class EmployeeDataCell: UITableViewCell {
+
+    // MARK: - Class Constants
+    static let identifier = "EmployeeDataCell"
+  
+    // MARK: - Outlets
+
+    @IBOutlet weak var lblEmpAge: UILabel!
+    @IBOutlet weak var lblEmpName: UILabel!
+    @IBOutlet weak var lblEmpSalary: UILabel!
+    @IBOutlet weak var empImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
